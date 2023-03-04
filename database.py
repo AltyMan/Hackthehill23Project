896145a47c99880c11aa32545f3aed06c8aee29b
Amit Altman <44475813+AltyMan@users.noreply.github.com>
2023-03-04 09:01:34 +0200

    DB creation and pull/push new tables

--- database.py
+++ database.py
@@ -1,5 +1,20 @@
 import mysql.connector
 
+email = "aaltman818@gmailcom".replace("@", "_") # for now no periods, may replace later with other character
+
 db = mysql.connector.connect(
-    
-)
+    host="XXX",
+    user="XXX",
+    passwd="XXX",
+    database="userFiles" # Add only after first run
+    )
+mycursor = db.cursor()
+
+# On first run
+mycursor.execute("CREATE DATABASE userFiles")
+
+# On second run
+mycursor.execute("CREATE TABLE " + email + " (file TEXT, fileID int PRIMARY KEY AUTO_INCREMENT)")
+
+# To view table
+mycursor.execute("DESCRIBE" + email)
