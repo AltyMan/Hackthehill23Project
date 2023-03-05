dcc55d01a944eabbb484dee0e7a16e3af2ba35f6
Amit Altman <44475813+AltyMan@users.noreply.github.com>
2023-03-05 08:57:19 +0200

    JS file update

--- static/index.js
+++ static/index.js
@@ -5,17 +5,6 @@ window.onload = function() {
   // createText("Read Me", "read-me-1", 1);
 }
 
-// document.getElementById("cards").onmousemove = e => {
-//     for(const card of document.getElementsByClassName("card")) {
-//       const rect = card.getBoundingClientRect(),
-//             x = e.clientX - rect.left,
-//             y = e.clientY - rect.top;
-  
-//       card.style.setProperty("--mouse-x", `${x}px`);
-//       card.style.setProperty("--mouse-y", `${y}px`);
-//     };
-//   }
-
 var gridElement = document.getElementById("grid");
 var elmHold = "";
 var lastSelected = "";
